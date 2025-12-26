961abda408d2e1e30bedf17bcf8ee250960ebaeb
zxie1272-ux <zxie1272@gmail.com>
2025-12-27 02:10:41 +0800

    feat(国际化): 添加确认中状态文本并更新iframe基础URL
    更新多语言文件添加"确认中"状态文本
    将iframe URL基础从VITE_SERVER_BASEURL改为VITE_FACE_BASEURL
    优化确认组件样式和国际化处理

--- src/components/confirm/index.vue
+++ src/components/confirm/index.vue
@@ -34,21 +34,34 @@ function onClose() {
 </script>
 
 <template>
-  <up-popup :show="props.show" mode="center" close-on-click-overlay :z-index="998" @close="onClose">
+  <up-popup
+    :show="props.show"
+    mode="center"
+    close-on-click-overlay
+    :z-index="998"
+    @close="onClose"
+  >
     <view class="bg1" :style="{ height: props.height }">
       <slot />
-      <div class="absolute bottom-[20px] w-full flex items-center justify-center">
+      <div
+        class="absolute bottom-[20px] w-full flex items-center justify-center"
+      >
         <!-- <view class="btn-block--white h-[40px] w-[40%]" @click="onClose">
           取消
         </view> -->
         <view class="btn-block ml-[5%] h-[40px] w-[70%]" @click="handleConfirm">
-          {{ props.buttonLoading ? '确认中' : '确认' }} <up-loading-icon
-            v-if="props.buttonLoading" size="14"
+          {{ props.buttonLoading ? $t("que-ren-zhong") : $t("que-ren") }}
+          <up-loading-icon
+            v-if="props.buttonLoading"
+            size="14"
             mode="semicircle"
           />
         </view>
       </div>
-      <div class="absolute bottom-[-50px] left-[50%] translate-x-[-50%]" @click="onClose">
+      <div
+        class="absolute bottom-[-50px] left-[50%] translate-x-[-50%]"
+        @click="onClose"
+      >
         <img class="h-[30px] w-[30px]" src="/static/server/close.png" alt="">
       </div>
     </view>
